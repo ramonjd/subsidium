88d47a341338a40a53272dbc173f461def88f86a
Ramon Dodd <ramon.dodd@sky.uk>
2016-01-04 14:02:49 +0000

    added cal

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,18 +1,39 @@
 if (process.env.BROWSER) {
   require('../styles/Dashboard.scss')
+  require('../styles/BigCalendar.scss')
+
+  // require('../../node_modules/react-big-calendar/lib/css/react-big-calendar.css')
 }
 
 import React, { Component, PropTypes } from 'react'
 // import Users from './Users'
-
-
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+import events from '../__events_test_data.js'
 
 export default class Dashboard extends Component {
+
+
+
+  constructor() {
+    super()
+    BigCalendar.momentLocalizer(moment)
+  }
 
   render() {
     return  (
       <div className="Dashboard">
-        TEST
+      <BigCalendar
+        selectable
+        events={events}
+        defaultView='month'
+        defaultDate={new Date(2015, 3, 1)}
+        onSelectEvent={event => alert(event.title)}
+        onSelectSlot={(slotInfo) => alert(
+          `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+          `\nend: ${slotInfo.end.toLocaleString()}`
+        )}
+        />
       </div>
       )
   }
